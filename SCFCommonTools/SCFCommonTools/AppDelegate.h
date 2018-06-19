20e2bb4046827e5afd4f0386ebac8585c3ad78b2
songchaofei <cf_olive@163.com>
2018-06-19 15:27:29 +0800

    创建新项目

---
+++ SCFCommonTools/SCFCommonTools/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SCFCommonTools
+//
+//  Created by 博颐 on 2018/6/19.
+//  Copyright © 2018年 scf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SCFCommonTools/SCFCommonTools/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SCFCommonTools
+//
+//  Created by 博颐 on 2018/6/19.
+//  Copyright © 2018年 scf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
