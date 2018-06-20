563d055648e03f5b54a50b60977cd1bb8ff6837f
songchaofei <cf_olive@163.com>
2018-06-20 17:23:06 +0800

    添加UIButton+SCFIndicator类别；

---
+++ SCFCommonTools/Categories/UIButton/UIButton+SCFIndicator.h
@@ -0,0 +1,22 @@
+//
+//  UIButton+SCFIndicator.h
+//  SCFCommonTools
+//
+//  Created by scf on 2018/6/20.
+//  Copyright © 2018年 scf. All rights reserved.
+//
+//  github: https://github.com/SUPER-F/SCFCommonTools
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIButton (SCFIndicator)
+
+// 使用活动指示器代替按钮的文本
+// 显示活动指示器，隐藏按钮文本
+- (void)scf_showIndicator;
+
+// 隐藏活动指示器，显示按钮文本
+- (void)scf_hideIndicator;
+
+@end
---
+++ SCFCommonTools/Categories/UIButton/UIButton+SCFIndicator.m
@@ -0,0 +1,51 @@
+//
+//  UIButton+SCFIndicator.m
+//  SCFCommonTools
+//
+//  Created by scf on 2018/6/20.
+//  Copyright © 2018年 scf. All rights reserved.
+//
+//  github: https://github.com/SUPER-F/SCFCommonTools
+//
+
+#import "UIButton+SCFIndicator.h"
+#import <objc/runtime.h>
+
+static NSString *const scf_buttonTextKey = @"scf_buttonTextKey";
+static NSString *const scf_indicatorViewKey = @"scf_indicatorViewKey";
+
+@implementation UIButton (SCFIndicator)
+
+- (void)scf_showIndicator {
+    //创建活动指示器
+    UIActivityIndicatorView *indicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhite];
+    indicator.center = CGPointMake(self.bounds.size.width / 2.0, self.bounds.size.height / 2.0);
+    [indicator startAnimating];
+    
+    // 获取当前按钮显示内容
+    NSString *currentButtonText = self.titleLabel.text;
+    
+    // 用runtime关联属性值
+    objc_setAssociatedObject(self, &scf_buttonTextKey, currentButtonText, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    objc_setAssociatedObject(self, &scf_indicatorViewKey, indicator, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    
+    // 置空按钮显示内容
+    [self setTitle:@"" forState:UIControlStateNormal];
+    // 禁止按钮点击
+    self.enabled = NO;
+    [self addSubview:indicator];
+}
+
+- (void)scf_hideIndicator {
+    NSString *currentButtonText = (NSString *)objc_getAssociatedObject(self, &scf_buttonTextKey);
+    UIActivityIndicatorView *indicator = (UIActivityIndicatorView *)objc_getAssociatedObject(self, &scf_indicatorViewKey);
+    
+    // 移除活动指示器
+    [indicator removeFromSuperview];
+    // 重置按钮显示内容
+    [self setTitle:currentButtonText forState:UIControlStateNormal];
+    // 开启按钮点击
+    self.enabled = YES;
+}
+
+@end
