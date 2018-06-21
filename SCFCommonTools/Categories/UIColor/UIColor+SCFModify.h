d0b82cbe53d261632bd83423780ae833f497d560
songchaofei <cf_olive@163.com>
2018-06-21 10:08:12 +0800

    添加UIColor+SCFModify 颜色修饰分类；

---
+++ SCFCommonTools/Categories/UIColor/UIColor+SCFModify.h
@@ -0,0 +1,39 @@
+//
+//  UIColor+SCFModify.h
+//  SCFCommonTools
+//
+//  Created by scf on 2018/6/21.
+//  Copyright © 2018年 scf. All rights reserved.
+//
+//  github: https://github.com/SUPER-F/SCFCommonTools
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (SCFModify)
+
+// 反向颜色
+- (UIColor *)scf_colorInverted;
+
+// 半透明颜色
+- (UIColor *)scf_colorTranslucency;
+
+
+/**
+ 颜色变亮
+
+ @param lighten 变亮参数
+ @return 变亮后的颜色
+ */
+- (UIColor *)scf_colorLighten:(CGFloat)lighten;
+
+
+/**
+ 颜色变暗
+
+ @param darken 变暗参数
+ @return 变暗后的颜色
+ */
+- (UIColor *)scf_colorDarken:(CGFloat)darken;
+
+@end
---
+++ SCFCommonTools/Categories/UIColor/UIColor+SCFModify.m
@@ -0,0 +1,76 @@
+//
+//  UIColor+SCFModify.m
+//  SCFCommonTools
+//
+//  Created by scf on 2018/6/21.
+//  Copyright © 2018年 scf. All rights reserved.
+//
+//  github: https://github.com/SUPER-F/SCFCommonTools
+//
+
+#import "UIColor+SCFModify.h"
+
+@implementation UIColor (SCFModify)
+
+- (UIColor *)scf_colorInverted {
+    NSArray *components = [self scf_componentArray];
+    return [UIColor colorWithRed:1 - [components[0] doubleValue]
+                           green:1 - [components[1] doubleValue]
+                            blue:1 - [components[2] doubleValue]
+                           alpha:[components[3] doubleValue]];
+}
+
+- (UIColor *)scf_colorTranslucency {
+    //指定HSB，参数是：色调（hue），饱和的（saturation），亮度（brightness）
+    CGFloat hue = 0, saturation = 0, brightness = 0, alpha = 0;
+    [self getHue:&hue saturation:&saturation brightness:&brightness alpha:&alpha];
+    
+    return [UIColor colorWithHue:hue
+                      saturation:saturation * 1.158
+                      brightness:brightness * 0.95
+                           alpha:alpha];
+}
+
+- (UIColor *)scf_colorLighten:(CGFloat)lighten {
+    //指定HSB，参数是：色调（hue），饱和的（saturation），亮度（brightness）
+    CGFloat hue = 0, saturation = 0, brightness = 0, alpha = 0;
+    [self getHue:&hue saturation:&saturation brightness:&brightness alpha:&alpha];
+    
+    return [UIColor colorWithHue:hue
+                      saturation:saturation * (1 - lighten)
+                      brightness:brightness * (1 + lighten)
+                           alpha:alpha];
+}
+
+- (UIColor *)scf_colorDarken:(CGFloat)darken {
+    //指定HSB，参数是：色调（hue），饱和的（saturation），亮度（brightness）
+    CGFloat hue = 0, saturation = 0, brightness = 0, alpha = 0;
+    [self getHue:&hue saturation:&saturation brightness:&brightness alpha:&alpha];
+    
+    return [UIColor colorWithHue:hue
+                      saturation:saturation * (1 + darken)
+                      brightness:brightness * (1 - darken)
+                           alpha:alpha];
+}
+
+#pragma mark - private methods
+- (NSArray *)scf_componentArray {
+    // 红色分量, 绿色分量, 蓝色分量, alpha分量
+    CGFloat red, green, blue, alpha;
+    const CGFloat *components = CGColorGetComponents([self CGColor]);
+    if (CGColorGetNumberOfComponents([self CGColor]) < 4) {
+        red = components[0];
+        green = components[0];
+        blue = components[0];
+        alpha = components[1];
+    }
+    else {
+        red = components[0];
+        green = components[1];
+        blue = components[2];
+        alpha = components[3];
+    }
+    return @[@(red), @(green), @(blue), @(alpha)];
+}
+
+@end
