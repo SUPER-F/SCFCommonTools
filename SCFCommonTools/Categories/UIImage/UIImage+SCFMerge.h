d5f37eaccad7a3d33b62fec9d32f09acb3e4b329
songchaofei <cf_olive@163.com>
2018-06-22 17:40:05 +0800

    添加 UIImage+SCFMerge 合并图片类别；

---
+++ SCFCommonTools/Categories/UIImage/UIImage+SCFMerge.h
@@ -0,0 +1,25 @@
+//
+//  UIImage+SCFMerge.h
+//  SCFCommonTools
+//
+//  Created by scf on 2018/6/22.
+//  Copyright © 2018年 scf. All rights reserved.
+//
+//  github: https://github.com/SUPER-F/SCFCommonTools
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (SCFMerge)
+
+
+/**
+ 合并两个图片
+
+ @param firstImage 第一个图片
+ @param secondImage 第二个图片
+ @return 合并后的图片
+ */
++ (UIImage *)scf_imageMergeImage:(UIImage *)firstImage withImage:(UIImage *)secondImage;
+
+@end
---
+++ SCFCommonTools/Categories/UIImage/UIImage+SCFMerge.m
@@ -0,0 +1,36 @@
+//
+//  UIImage+SCFMerge.m
+//  SCFCommonTools
+//
+//  Created by scf on 2018/6/22.
+//  Copyright © 2018年 scf. All rights reserved.
+//
+//  github: https://github.com/SUPER-F/SCFCommonTools
+//
+
+#import "UIImage+SCFMerge.h"
+
+@implementation UIImage (SCFMerge)
+
++ (UIImage *)scf_imageMergeImage:(UIImage *)firstImage withImage:(UIImage *)secondImage {
+    CGImageRef firstImageRef = firstImage.CGImage;
+    CGFloat firstWidth = CGImageGetWidth(firstImageRef);
+    CGFloat firstHeight = CGImageGetHeight(firstImageRef);
+    
+    CGImageRef secondImageRef = secondImage.CGImage;
+    CGFloat secondWidth = CGImageGetWidth(secondImageRef);
+    CGFloat secondHeight = CGImageGetHeight(secondImageRef);
+    
+    CGSize mergeSize = CGSizeMake(MAX(firstWidth, secondWidth), MAX(firstHeight, secondHeight));
+    
+    UIGraphicsBeginImageContext(mergeSize);
+    [firstImage drawInRect:CGRectMake(0.0f, 0.0f, firstWidth, firstHeight)];
+    [secondImage drawInRect:CGRectMake(0.0f, 0.0f, secondWidth, secondHeight)];
+    
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return image;
+}
+
+@end
