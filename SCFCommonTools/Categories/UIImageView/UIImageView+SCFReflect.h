f59f3f629caeeb0202bf955a990595baffc37c25
songchaofei <cf_olive@163.com>
2018-06-26 17:28:37 +0800

    添加 UIImageView+SCFReflect 倒影的类别；

---
+++ SCFCommonTools/Categories/UIImageView/UIImageView+SCFReflect.h
@@ -0,0 +1,21 @@
+//
+//  UIImageView+SCFReflect.h
+//  SCFCommonTools
+//
+//  Created by scf on 2018/6/26.
+//  Copyright © 2018年 scf. All rights reserved.
+//
+//  github: https://github.com/SUPER-F/SCFCommonTools
+//
+//
+//  https://github.com/zzz6519003/UIImageView-Reflect
+//  A simple category that creates a relection view for your UIImageView.
+
+#import <UIKit/UIKit.h>
+
+@interface UIImageView (SCFReflect)
+
+// 倒影
+- (void)scf_reflect;
+
+@end
---
+++ SCFCommonTools/Categories/UIImageView/UIImageView+SCFReflect.m
@@ -0,0 +1,41 @@
+//
+//  UIImageView+SCFReflect.m
+//  SCFCommonTools
+//
+//  Created by scf on 2018/6/26.
+//  Copyright © 2018年 scf. All rights reserved.
+//
+//  github: https://github.com/SUPER-F/SCFCommonTools
+//
+
+#import "UIImageView+SCFReflect.h"
+
+@implementation UIImageView (SCFReflect)
+
+- (void)scf_reflect {
+    CGRect frame = self.frame;
+    frame.origin.y += (frame.size.height + 1);
+    
+    UIImageView *reflectionImageView = [[UIImageView alloc] initWithFrame:frame];
+    self.clipsToBounds = YES;
+    reflectionImageView.contentMode = self.contentMode;
+    reflectionImageView.image = self.image;
+    reflectionImageView.transform = CGAffineTransformMakeScale(1.0, -1.0);
+    
+    CALayer *reflectionLayer = reflectionImageView.layer;
+    
+    CAGradientLayer *gradientLayer = [CAGradientLayer layer];
+    gradientLayer.bounds = reflectionLayer.bounds;
+    gradientLayer.position = CGPointMake(reflectionLayer.bounds.size.width / 2.0f, reflectionLayer.bounds.size.height / 2.0f);
+    gradientLayer.colors = [NSArray arrayWithObjects:
+                            (id)[[UIColor clearColor] CGColor],
+                            (id)[[UIColor colorWithRed:1.0f green:1.0f blue:1.0f alpha:0.3f] CGColor],
+                            nil];
+    gradientLayer.startPoint = CGPointMake(0.5, 0.5);
+    gradientLayer.endPoint = CGPointMake(0.5, 1.0);
+    reflectionLayer.mask = gradientLayer;
+    
+    [self.superview addSubview:reflectionImageView];
+}
+
+@end
