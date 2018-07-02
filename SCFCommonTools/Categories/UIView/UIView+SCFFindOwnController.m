620014ebdb1bbe6764d50f9c2cb1c8eb257a2569
songchaofei <cf_olive@163.com>
2018-07-02 14:28:46 +0800

    添加 UIView+SCFFindOwnController 找到自己的viewcontroller的类别；

---
+++ SCFCommonTools/Categories/UIView/UIView+SCFFindOwnController.m
@@ -0,0 +1,25 @@
+//
+//  UIView+SCFFindOwnController.m
+//  SCFCommonTools
+//
+//  Created by scf on 2018/7/2.
+//  Copyright © 2018年 scf. All rights reserved.
+//
+//  github: https://github.com/SUPER-F/SCFCommonTools
+//
+
+#import "UIView+SCFFindOwnController.h"
+
+@implementation UIView (SCFFindOwnController)
+
+- (UIViewController *)scf_findOwnController {
+    for (UIView* next = self; next; next = next.superview) {
+        UIResponder* nextResponder = [next nextResponder];
+        if ([nextResponder isKindOfClass:[UIViewController class]]) {
+            return (UIViewController*)nextResponder;
+        }
+    }
+    return nil;
+}
+
+@end
