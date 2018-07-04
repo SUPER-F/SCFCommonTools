39e216434f2f6277093344892b31349a61a09016
songchaofei <cf_olive@163.com>
2018-07-04 17:48:55 +0800

    添加 NSString+SCFString 类别；

---
+++ SCFCommonTools/Categories/NSString/NSString+SCFString.h
@@ -0,0 +1,33 @@
+//
+//  NSString+SCFString.h
+//  SCFCommonTools
+//
+//  Created by scf on 2018/7/4.
+//  Copyright © 2018年 scf. All rights reserved.
+//
+//  github: https://github.com/SUPER-F/SCFCommonTools
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (SCFString)
+
+//是否是空
++ (BOOL)isEmpty:(NSString*)string;
+
+//判断两个字符串是否相等
++ (BOOL)isString:(NSString*)s1 equalToString:(NSString*)s2;
+
+//字符串是否包含emoji表情
++ (BOOL)isContainsEmojiFromString:(NSString *)string;
+
+/**
+ *  中文和中文符号算2个，英文和英文符号算1个，空格算1个
+ *
+ *  @param strtemp 字符串
+ *
+ *  @return 长度
+ */
++ (NSUInteger)stringLenght:(NSString*)strtemp;
+
+@end
---
+++ SCFCommonTools/Categories/NSString/NSString+SCFString.m
@@ -0,0 +1,94 @@
+//
+//  NSString+SCFString.m
+//  SCFCommonTools
+//
+//  Created by scf on 2018/7/4.
+//  Copyright © 2018年 scf. All rights reserved.
+//
+//  github: https://github.com/SUPER-F/SCFCommonTools
+//
+
+#import "NSString+SCFString.h"
+
+@implementation NSString (SCFString)
+
++ (BOOL)isEmpty:(NSString *)string {
+    if((NSNull*)string == [NSNull null]) {
+        return YES;
+    }
+    
+    NSString *newstr;
+    if(string){
+        newstr = [string stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+    }
+    if(!newstr || [newstr isEqualToString:@""]) {
+        return YES;
+    }
+    
+    return NO;
+}
+
++ (BOOL)isString:(NSString *)s1 equalToString:(NSString *)s2 {
+    if ([NSString isEmpty:s1] && [NSString isEmpty:s2]){
+        return YES;
+    }
+    else if([s1 isEqualToString:s2]){
+        return YES;
+    }
+    return NO;
+}
+
++ (BOOL)isContainsEmojiFromString:(NSString *)string {
+    __block BOOL returnValue = NO;
+    
+    [string enumerateSubstringsInRange:NSMakeRange(0, [string length])
+                               options:NSStringEnumerationByComposedCharacterSequences
+                            usingBlock:^(NSString *substring, NSRange substringRange, NSRange enclosingRange, BOOL *stop) {
+                                const unichar hs = [substring characterAtIndex:0];
+                                if (0xd800 <= hs && hs <= 0xdbff) {
+                                    if (substring.length > 1) {
+                                        const unichar ls = [substring characterAtIndex:1];
+                                        const int uc = ((hs - 0xd800) * 0x400) + (ls - 0xdc00) + 0x10000;
+                                        if (0x1d000 <= uc && uc <= 0x1f77f) {
+                                            returnValue = YES;
+                                        }
+                                    }
+                                } else if (substring.length > 1) {
+                                    const unichar ls = [substring characterAtIndex:1];
+                                    if (ls == 0x20e3) {
+                                        returnValue = YES;
+                                    }
+                                } else {
+                                    if (0x2100 <= hs && hs <= 0x27ff) {
+                                        returnValue = YES;
+                                    } else if (0x2B05 <= hs && hs <= 0x2b07) {
+                                        returnValue = YES;
+                                    } else if (0x2934 <= hs && hs <= 0x2935) {
+                                        returnValue = YES;
+                                    } else if (0x3297 <= hs && hs <= 0x3299) {
+                                        returnValue = YES;
+                                    } else if (hs == 0xa9 || hs == 0xae || hs == 0x303d || hs == 0x3030 || hs == 0x2b55 || hs == 0x2b1c || hs == 0x2b1b || hs == 0x2b50) {
+                                        returnValue = YES;
+                                    }
+                                }
+                            }];
+    
+    return returnValue;
+}
+
++ (NSUInteger)stringLenght:(NSString *)strtemp {
+    NSUInteger strlength = 0;
+    char* p = (char*)[strtemp cStringUsingEncoding:NSUnicodeStringEncoding];
+    for (NSUInteger i = 0; i < [strtemp lengthOfBytesUsingEncoding:NSUnicodeStringEncoding]; i++) {
+        if (*p) {
+            p++;
+            strlength++;
+        }
+        else {
+            p++;
+        }
+    }
+    return strlength;
+}
+
+@end
