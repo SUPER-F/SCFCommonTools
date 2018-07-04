6174ac4a1ce51cf6a319239f109c55e68d112fc8
songchaofei <cf_olive@163.com>
2018-07-04 16:47:36 +0800

    添加 NSString+SCFBase64 类别；

---
+++ SCFCommonTools/Categories/NSString/NSString+SCFBase64.h
@@ -0,0 +1,19 @@
+//
+//  NSString+SCFBase64.h
+//  SCFCommonTools
+//
+//  Created by scf on 2018/7/4.
+//  Copyright © 2018年 scf. All rights reserved.
+//
+//  github: https://github.com/SUPER-F/SCFCommonTools
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (SCFBase64)
+
++ (NSString *)scf_base64EncodedStringFromString:(NSString *)fromString;
+
++ (NSString *)scf_stringFromBase64EncodedString:(NSString *)base64String;
+
+@end
---
+++ SCFCommonTools/Categories/NSString/NSString+SCFBase64.m
@@ -0,0 +1,25 @@
+//
+//  NSString+SCFBase64.m
+//  SCFCommonTools
+//
+//  Created by scf on 2018/7/4.
+//  Copyright © 2018年 scf. All rights reserved.
+//
+//  github: https://github.com/SUPER-F/SCFCommonTools
+//
+
+#import "NSString+SCFBase64.h"
+
+@implementation NSString (SCFBase64)
+
++ (NSString *)scf_base64EncodedStringFromString:(NSString *)fromString {
+    NSData *data = [fromString dataUsingEncoding:NSUTF8StringEncoding];
+    return [data base64EncodedStringWithOptions:0];
+}
+
++ (NSString *)scf_stringFromBase64EncodedString:(NSString *)base64String {
+    NSData *data = [[NSData alloc] initWithBase64EncodedString:base64String options:0];
+    return [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+}
+
+@end
