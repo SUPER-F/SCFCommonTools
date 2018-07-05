aaf64514079cde1796fc2d3098af6985e1383529
songchaofei <cf_olive@163.com>
2018-07-05 18:36:07 +0800

    添加 NSDictionary+SCFURLParameters 类别；

---
+++ SCFCommonTools/Categories/NSDictionary/NSDictionary+SCFURLParameters.h
@@ -0,0 +1,32 @@
+//
+//  NSDictionary+SCFURLParameters.h
+//  SCFCommonTools
+//
+//  Created by scf on 2018/7/5.
+//  Copyright © 2018年 scf. All rights reserved.
+//
+//  github: https://github.com/SUPER-F/SCFCommonTools
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDictionary (SCFURLParameters)
+
+
+/**
+ 将url参数转换成NSDictionary
+
+ @param urlString 待有参数的URL
+ @return 参数字典
+ */
++ (NSDictionary *)scf_dictionaryWithURLString:(NSString *)urlString;
+
+
+/**
+ 将NSDictionary转换成url 参数字符串
+
+ @return URL参数字符串
+ */
+- (NSString *)scf_urlParametersString;
+
+@end
---
+++ SCFCommonTools/Categories/NSDictionary/NSDictionary+SCFURLParameters.m
@@ -0,0 +1,49 @@
+//
+//  NSDictionary+SCFURLParameters.m
+//  SCFCommonTools
+//
+//  Created by scf on 2018/7/5.
+//  Copyright © 2018年 scf. All rights reserved.
+//
+//  github: https://github.com/SUPER-F/SCFCommonTools
+//
+
+#import "NSDictionary+SCFURLParameters.h"
+
+@implementation NSDictionary (SCFURLParameters)
+
++ (NSDictionary *)scf_dictionaryWithURLString:(NSString *)urlString {
+    NSMutableDictionary *dict = [NSMutableDictionary dictionary];
+    NSArray *parameters = [urlString componentsSeparatedByString:@"&"];
+    for(NSString *parameter in parameters) {
+        NSArray *contents = [parameter componentsSeparatedByString:@"="];
+        if([contents count] == 2) {
+            NSString *key = [contents objectAtIndex:0];
+            NSString *value = [contents objectAtIndex:1];
+            value = [value stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+            if (key && value) {
+                [dict setObject:value forKey:key];
+            }
+        }
+    }
+    return [NSDictionary dictionaryWithDictionary:dict];
+}
+
+- (NSString *)scf_urlParametersString {
+    NSMutableString *string = [NSMutableString string];
+    for (NSString *key in [self allKeys]) {
+        if ([string length]) {
+            [string appendString:@"&"];
+        }
+        CFStringRef escaped = CFURLCreateStringByAddingPercentEscapes(NULL,
+                                                                      (CFStringRef)[[self objectForKey:key] description],
+                                                                      NULL,
+                                                                      (CFStringRef)@"!*'();:@&=+$,/?%#[]",
+                                                                      kCFStringEncodingUTF8);
+        [string appendFormat:@"%@=%@", key, escaped];
+        CFRelease(escaped);
+    }
+    return string;
+}
+
+@end
