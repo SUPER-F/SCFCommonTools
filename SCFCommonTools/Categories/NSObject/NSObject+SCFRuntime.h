c32f3d8638038422ea37dd7600b9162e50ee31a7
songchaofei <cf_olive@163.com>
2018-07-09 15:32:29 +0800

    添加 NSObject+SCFRuntime 类别；

---
+++ SCFCommonTools/Categories/NSObject/NSObject+SCFRuntime.h
@@ -0,0 +1,57 @@
+//
+//  NSObject+SCFRuntime.h
+//  SCFCommonTools
+//
+//  Created by scf on 2018/7/9.
+//  Copyright © 2018年 scf. All rights reserved.
+//
+//  github: https://github.com/SUPER-F/SCFCommonTools
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (SCFRuntime)
+
+/**
+ swizzle 类方法
+ 
+ @param oriSel 原有的方法
+ @param swiSel swizzle的方法
+ */
++ (void)scf_swizzleClassMethodWithOriginSel:(SEL)oriSel swizzledSel:(SEL)swiSel;
+
+/**
+ swizzle 实例方法
+ 
+ @param oriSel 原有的方法
+ @param swiSel swizzle的方法
+ */
++ (void)scf_swizzleInstanceMethodWithOriginSel:(SEL)oriSel swizzledSel:(SEL)swiSel;
+
+/**
+ 动态创建绑定selector的类
+ tip：每当无法找到selectorcrash转发过来的所有selector都会追加到当前Class上
+ 
+ @param aSelector 传入selector
+ @return 返回创建的类
+ */
++ (Class)scf_addMethodToStubClass:(SEL)aSelector;
+
+/**
+ 判断当前类是否在主bundle里
+ 
+ @param cls 出入类
+ @return 返回判断结果
+ */
++ (BOOL)scf_isMainBundleClass:(Class)cls;
+
+/**
+ 判断方法是否在子类里override了
+ 
+ @param cls 传入要判断的Class
+ @param sel 传入要判断的Selector
+ @return 返回判断是否被重载的结果
+ */
+- (BOOL)scf_isMethodOverrideForClass:(Class)cls selector:(SEL)sel;
+
+@end
